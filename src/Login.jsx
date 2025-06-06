42ff05ca44ab49cbce7e6fbde2e336ba53bb600d
Vitor Lucena <104996449+vitorlucena1@users.noreply.github.com>
2025-06-06 12:12:11 +0000

    feat: cria componentes básicos de Login e Registro

---
+++ src/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div className="auth-container">
+      <h2>Login</h2>
+      {/* Formulário será adicionado depois */}
+    </div>
+  )
+}
+
+export default Login
---
+++ src/Register.jsx
@@ -0,0 +1,10 @@
+function Register() {
+  return (
+    <div className="auth-container">
+      <h2>Registro</h2>
+      {/* Formulário será adicionado depois */}
+    </div>
+  )
+}
+
+export default Register
