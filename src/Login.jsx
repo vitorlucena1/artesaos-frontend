0f911ba21cf24d15a572ec4cfcb2ab6bddede3ae
Vitor Lucena <104996449+vitorlucena1@users.noreply.github.com>
2025-06-06 12:33:24 +0000

    feat: implementa formulário de login com campos de e-mail e senha style: ajusta largura máxima do container e corrige cores no CSS

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,7 +2,17 @@ function Login() {
   return (
     <div className="auth-container">
       <h2>Login</h2>
-      {/* Formulário será adicionado depois */}
+      <form>
+        <div>
+          <label htmlFor="email">E-mail:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="password">Senha:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
     </div>
   )
 }
